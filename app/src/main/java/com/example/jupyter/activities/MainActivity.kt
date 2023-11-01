495bab8f124dd03bb334d7cdf14909d961a5a4e4
Mateusz <potasnik@student.agh.edu.pl>
2023-11-01 12:13:57 +0100

    dimens bug workaround

--- app/src/main/java/com/example/jupyter/activities/MainActivity.kt
+++ app/src/main/java/com/example/jupyter/activities/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.jupyter.activities
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.jupyter.R
 import java.io.File
 
 class MainActivity : ComponentActivity() {
+//    private val createButton: ImageButton? = findViewById(R.id.addNotebookButton)
+//    private val listView: LinearLayout? = findViewById(R.id.notebook_list)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            setContentView(R.layout.activity_notebook)
+            setContentView(R.layout.activity_main)
         }
+//        createButton?.setOnClickListener {
+//            createIpynbFile()
+//            if (listView != null) {
+//                listIpynbFiles(listView)
+//            }
+//        }
+//
+//        if (listView != null) {
+//            listIpynbFiles(listView)
+//        }
     }
     private fun createIpynbFile() {
         val fileName = "notebook_${System.currentTimeMillis()}.ipynb"
@@ -29,10 +46,20 @@ class MainActivity : ComponentActivity() {
         """.trimIndent())
     }
 
-    private fun listIpynbFiles(listView: ListView) {
-        val files = filesDir.listFiles { _, name -> name.endsWith(".ipynb") }?.map { it.name } ?: listOf()
+    private fun listIpynbFiles(linearLayout: LinearLayout) {
+        val files = filesDir.listFiles { _, name -> name.endsWith(".ipynb") } ?: return
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, files)
-        listView.adapter = adapter
+        linearLayout.removeAllViews()
+
+        val inflater = LayoutInflater.from(this)
+        for (file in files) {
+
+            val notebookView = inflater.inflate(R.layout.notebook_item, linearLayout, false)
+
+            val textView = notebookView.findViewById<TextView>(R.id.notebook_name)
+            textView.text = file.name
+
+            linearLayout.addView(notebookView)
+        }
     }
 }
